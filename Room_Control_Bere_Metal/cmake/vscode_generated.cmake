cc74a8e675119f021b2ca68adf1fc77a39605839
evhernandezt <evhernandezt@unal.edu.co>
2025-10-09 12:25:48 -0500

    actulizaciones

--- Room_Control_Bere_Metal/cmake/vscode_generated.cmake
+++ Room_Control_Bere_Metal/cmake/vscode_generated.cmake
@@ -23,11 +23,11 @@ set(linker_script_SRC ${linker_script_SRC}
 # Sources
 set(sources_SRCS ${sources_SRCS}
 
-	${CMAKE_CURRENT_SOURCE_DIR}/Src/main_t.c
+	#${CMAKE_CURRENT_SOURCE_DIR}/Src/main_t.c
 	${CMAKE_CURRENT_SOURCE_DIR}/Src/rom_control.c
     ${CMAKE_CURRENT_SOURCE_DIR}/Src/tim.c
 	${CMAKE_CURRENT_SOURCE_DIR}/Src/nvic.c
-	#${CMAKE_CURRENT_SOURCE_DIR}/Src/main.c
+	${CMAKE_CURRENT_SOURCE_DIR}/Src/main.c
 	${CMAKE_CURRENT_SOURCE_DIR}/Src/gpio.c
 	${CMAKE_CURRENT_SOURCE_DIR}/Src/rcc.c
 	${CMAKE_CURRENT_SOURCE_DIR}/Src/systick.c
